ad001aa504d91a3a2071df57285e1b54e40b7db1
Fernando Weiss <fernandoviniciuswr@gmail.com>
2020-07-13 16:07:45 -0300

    commit some care with numbers
    Alguns cuidados com os números em js.

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,10 @@
+console.log(7 / 0)       //Infinito
+
+console.log("10.2" / 2)  //Consegue ser convertido e é realizado a operação
+
+console.log("Show!" * 2) //Retorna NaN, NÃO multiplica a String
+
+console.log(0.1 + 0.7)   //Retorna 0.79 por causa do padrão I3E, não é 100% preciso
+
+// console.log(10.toString()) números literais não funcioanm as funções
+console.log((10).toString()) // Porém com os parênteses funciona
