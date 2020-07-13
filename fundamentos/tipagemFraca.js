9d4318bdd777aa13afdcde38ac34a537311556fc
Fernando Weiss <fernandoviniciuswr@gmail.com>
2020-07-13 15:51:43 -0300

    commit weak typing
    Mostrando como pode se mudar o tipo de uma váriavel.

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+let anything = 'cool'
+
+console.log(anything)
+console.log(typeof anything)
+
+anything = 3.5
+console.log(anything)
+console.log(typeof anything)
+
+//Evitar nomes genéricos e siglas Ex: valor, número, pqp(Produto químico perígoso)
