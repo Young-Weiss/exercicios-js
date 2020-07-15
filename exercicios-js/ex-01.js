cc0520e4819b3ed593c00d0f1ba5a4032a01dd6d
Fernando Weiss <fernandoviniciuswr@gmail.com>
2020-07-14 23:44:16 -0300

    commit exercices 1, 2, 3
    Adcionando os 3 primeiros exercícios.

---
+++ exercicios-js/ex-01.js
@@ -0,0 +1,13 @@
+/*
+01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.
+*/
+
+function soma(a, b){
+    console.log(`${a} + ${b} = ${a + b}`)
+    console.log(`${a} - ${b} = ${a - b}`)
+    console.log(`${a} * ${b} = ${a * b}`)
+    console.log(`${a} / ${b} = ${a / b}`)
+}
+
+soma(1, 2)
---
+++ exercicios-js/ex-03.js
@@ -0,0 +1,9 @@
+/*
+03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente
+*/
+
+function potencia(num, exp){
+    return Math.pow(num, exp)
+}
+
+console.log(potencia(2, 3))
