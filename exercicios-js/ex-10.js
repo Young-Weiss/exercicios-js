103184ca87e59b2718e0536caa024db8feba606f
Fernando Weiss <fernandoviniciuswr@gmail.com>
2020-07-20 14:30:37 -0300

    commit execises 10 to 15
    Execícios concluídos do 10 ao 15.

---
+++ exercicios-js/ex-10.js
@@ -0,0 +1,9 @@
+/* 
+10) Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
+ou false
+ */
+
+divisivelPorTres = num => num % 3 === 0 ? true : false
+
+console.log(divisivelPorTres(23))
+
---
+++ exercicios-js/ex-11.js
@@ -0,0 +1,15 @@
+/* 
+11) As regras para o cálculo dos anos bissextos são as seguintes:
+De 4 em 4 anos é ano bissexto;
+De 100 em 100 anos não é ano bissexto;
+De 400 em 400 anos é ano bissexto;
+Prevalecem as últimas regras sobre as primeiras.
+Partindo daí elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a
+mensagem e retornando true ou false.
+ */
+
+// Os seguintes anos são bissextos: 1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052
+
+ const anoBissexto = ano => ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))) ? true : false
+
+ console.log(anoBissexto(1993))
